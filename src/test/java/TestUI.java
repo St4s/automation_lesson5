98e7ff71ea3bda7075a5af8086ecf4663aa3a105
Stas Zakharkevich <zakharkevitch.stas@gmail.com>
2020-11-18 23:20:50 +0300

    added tests

--- src/test/java/TestUI.java
+++ src/test/java/TestUI.java
@@ -6,6 +6,117 @@ import org.testng.annotations.Test;
 
 public class TestUI {
     @Test
-    public void test1() {
+    public void test1(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://healthunify.com/bmicalculator/");
+
+        driver.findElement(By.name("wg")).sendKeys("123");
+        driver.findElement(By.name("ht")).sendKeys("123");
+        driver.findElement(By.name("cc")).click();
+        WebElement out = driver.findElement(By.name("desc"));
+        String outText = out.getAttribute("value");
+        System.out.println(outText);
+
+        driver.close();
+
     }
+
+    @Test
+    public void test2(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://healthunify.com/bmicalculator/");
+
+        driver.findElement(By.name("wg")).sendKeys("75");
+        driver.findElement(By.name("ht")).sendKeys("180");
+        driver.findElement(By.name("cc")).click();
+        WebElement out = driver.findElement(By.name("desc"));
+        String outText = out.getAttribute("value");
+        System.out.println(outText);
+
+        driver.close();
+
+    }
+
+    @Test
+    public void test3(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://healthunify.com/bmicalculator/");
+
+        driver.findElement(By.name("wg")).sendKeys("50");
+        driver.findElement(By.name("ht")).sendKeys("180");
+        driver.findElement(By.name("cc")).click();
+        WebElement out = driver.findElement(By.name("desc"));
+        String outText = out.getAttribute("value");
+        System.out.println(outText);
+
+        driver.close();
+
+    }
+
+    @Test
+    public void test4(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://healthunify.com/bmicalculator/");
+
+        driver.findElement(By.name("wg")).sendKeys("25");
+        driver.findElement(By.name("ht")).sendKeys("180");
+        driver.findElement(By.name("cc")).click();
+        WebElement out = driver.findElement(By.name("desc"));
+        String outText = out.getAttribute("value");
+        System.out.println(outText);
+
+        driver.close();
+
+    }
+
+    @Test
+    public void test5(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://healthunify.com/bmicalculator/");
+
+        driver.findElement(By.name("wg")).sendKeys("25");
+        driver.findElement(By.name("ht")).sendKeys("180");
+        driver.findElement(By.name("cc")).click();
+        WebElement out = driver.findElement(By.name("desc"));
+        String outText = out.getAttribute("readonly");
+        System.out.println(outText);
+
+        driver.close();
+
+    }
+
+    @Test
+    public void test6(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://healthunify.com/bmicalculator/");
+
+        driver.findElement(By.name("wg")).sendKeys("75");
+        driver.findElement(By.name("ht")).sendKeys("180");
+        driver.findElement(By.name("cc")).click();
+        WebElement out = driver.findElement(By.name("si"));
+        String outText = out.getAttribute("value");
+        System.out.println(outText);
+
+        driver.close();
+
+    }
+
 }
